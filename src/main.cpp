321f3ec339aaca48084b69690da913aa2891ab25
LegendPhoenix66 <64265723+LegendPhoenix66@users.noreply.github.com>
2025-08-20 21:50:24 +0200

    Refactor `main` function to improve modularity and add server initialization
    - Extract argument validation into `check_args` for cleaner logic.
    - Introduce `init_config` and `start_server` methods to separate configuration parsing and server setup.
    - Support initialization of multiple configurations for future scalability.

--- src/main.cpp
+++ src/main.cpp
@@ -1,78 +1,100 @@
 #include "../inc/WebServ.hpp"
 #include "../inc/ParseConfig.hpp"
 
-void	print_conf(const ServerConfig &conf)
-{
-	std::cout << "Server Configuration:" << std::endl;
-	std::cout << "Port: " << conf.getPort() << std::endl;
-	std::cout << "Server Name: " << conf.getServerName() << std::endl;
-	std::cout << "Host: " << conf.getHost() << std::endl;
-	std::cout << "Root: " << conf.getRoot() << std::endl;
-	std::cout << "Index: ";
-	for (size_t i = 0; i < conf.getIndex().size(); i++)
-		std::cout << conf.getIndex()[i] << " ";
-	std::cout << std::endl;
-	const std::vector<Location> locations = conf.getLocations();
-	std::cout << "Locations: " << std::endl;
-	for (size_t i = 0; i < locations.size(); i++) {
-		const Location &loc = locations[i];
-		std::cout << "  Path: " << loc.getPath() << std::endl;
-		if (!loc.getRoot().empty())
-			std::cout << "  Root: " << loc.getRoot() << std::endl;
-		if (!loc.getCgiPass().empty())
-			std::cout << "  CGI Pass: " << loc.getCgiPass() << std::endl;
-		if (!loc.getCgiPath().empty())
-			std::cout << "  CGI Path: " << loc.getCgiPath() << std::endl;
-		std::cout << "  Autoindex: " << (loc.getAutoindex() ? "on" : "off") << std::endl;
-		if (!loc.getIndex().empty()) {
-			std::cout << "  Index: ";
-			for (size_t j = 0; j < loc.getIndex().size(); j++)
-				std::cout << loc.getIndex()[j] << " ";
-			std::cout << std::endl;
-		}
-		if (!loc.getAllowedMethods().empty()) {
-			std::cout << "  Allowed Methods: ";
-			for (size_t j = 0; j < loc.getAllowedMethods().size(); j++)
-				std::cout << loc.getAllowedMethods()[j] << " ";
-			std::cout << std::endl;
-		}
-		std::cout << "  Return Directory: ";
-		if (loc.getReturnDir().first != 0)
-			std::cout << loc.getReturnDir().first << " ";
-		if (!loc.getReturnDir().second.empty())
-			std::cout << loc.getReturnDir().second;
-		std::cout << std::endl;
-		if (loc.getReturnDir().second.empty() && loc.getReturnDir().first != 0)
-			std::cout << "  Return Directory: " << loc.getReturnDir().first << std::endl;
-		else if (loc.getReturnDir().first == 0 && !loc.getReturnDir().second.empty())
-			std::cout << "  Return Directory: " << loc.getReturnDir().second << std::endl;
-		else if (loc.getReturnDir().first != 0 && !loc.getReturnDir().second.empty())
-			std::cout << "  Return Directory: " << loc.getReturnDir().first << " " << loc.getReturnDir().second << std::endl;
-		if (!loc.getUploadStore().empty())
-			std::cout << "  Upload Store: " << loc.getUploadStore() << std::endl;
-		std::cout << "----------------------------------------" << std::endl;
-	}
-	const std::map<int, std::string> error_pages = conf.getErrorPages();
-	std::cout << "Error Pages: " << std::endl;
-	for (std::map<int, std::string>::const_iterator it = error_pages.begin(); it != error_pages.end(); ++it) {
-		if (!it->second.empty())
-			std::cout << "  " << it->first << ": " << it->second << std::endl;
-	}
+void print_conf(const ServerConfig &conf) {
+    std::cout << "Server Configuration:" << std::endl;
+    std::cout << "Port: " << conf.getPort() << std::endl;
+    std::cout << "Server Name: " << conf.getServerName() << std::endl;
+    std::cout << "Host: " << conf.getHost() << std::endl;
+    std::cout << "Root: " << conf.getRoot() << std::endl;
+    std::cout << "Index: ";
+    for (size_t i = 0; i < conf.getIndex().size(); i++)
+        std::cout << conf.getIndex()[i] << " ";
+    std::cout << std::endl;
+    const std::vector<Location> locations = conf.getLocations();
+    std::cout << "Locations: " << std::endl;
+    for (size_t i = 0; i < locations.size(); i++) {
+        const Location &loc = locations[i];
+        std::cout << "  Path: " << loc.getPath() << std::endl;
+        if (!loc.getRoot().empty())
+            std::cout << "  Root: " << loc.getRoot() << std::endl;
+        if (!loc.getCgiPass().empty())
+            std::cout << "  CGI Pass: " << loc.getCgiPass() << std::endl;
+        if (!loc.getCgiPath().empty())
+            std::cout << "  CGI Path: " << loc.getCgiPath() << std::endl;
+        std::cout << "  Autoindex: " << (loc.getAutoindex() ? "on" : "off") << std::endl;
+        if (!loc.getIndex().empty()) {
+            std::cout << "  Index: ";
+            for (size_t j = 0; j < loc.getIndex().size(); j++)
+                std::cout << loc.getIndex()[j] << " ";
+            std::cout << std::endl;
+        }
+        if (!loc.getAllowedMethods().empty()) {
+            std::cout << "  Allowed Methods: ";
+            for (size_t j = 0; j < loc.getAllowedMethods().size(); j++)
+                std::cout << loc.getAllowedMethods()[j] << " ";
+            std::cout << std::endl;
+        }
+        std::cout << "  Return Directory: ";
+        if (loc.getReturnDir().first != 0)
+            std::cout << loc.getReturnDir().first << " ";
+        if (!loc.getReturnDir().second.empty())
+            std::cout << loc.getReturnDir().second;
+        std::cout << std::endl;
+        if (loc.getReturnDir().second.empty() && loc.getReturnDir().first != 0)
+            std::cout << "  Return Directory: " << loc.getReturnDir().first << std::endl;
+        else if (loc.getReturnDir().first == 0 && !loc.getReturnDir().second.empty())
+            std::cout << "  Return Directory: " << loc.getReturnDir().second << std::endl;
+        else if (loc.getReturnDir().first != 0 && !loc.getReturnDir().second.empty())
+            std::cout << "  Return Directory: " << loc.getReturnDir().first << " " << loc.getReturnDir().second
+                      << std::endl;
+        if (!loc.getUploadStore().empty())
+            std::cout << "  Upload Store: " << loc.getUploadStore() << std::endl;
+        std::cout << "----------------------------------------" << std::endl;
+    }
+    const std::map<int, std::string> error_pages = conf.getErrorPages();
+    std::cout << "Error Pages: " << std::endl;
+    for (std::map<int, std::string>::const_iterator it = error_pages.begin(); it != error_pages.end(); ++it) {
+        if (!it->second.empty())
+            std::cout << "  " << it->first << ": " << it->second << std::endl;
+    }
 }
 
-int main(int argc, char **argv) {
+int check_args(int argc) {
     if (argc < 2) {
         std::cerr << "Error: No config file specified.\nUsage: ./webserv <config_file>" << std::endl;
         return 1;
     }
+    return 0;
+}
 
-	try {
-		ServerConfig config = ParseConfig(argv[1]).getConfig();
-		print_conf(config);
-	}
-	catch (std::exception &e) {
-		std::cerr << "Error: " << e.what() << std::endl;
-	}
+// support for multiple config files
+//std::vector<ServerConfig> init_config(char *path) {
+//	return ParseConfig(path).getConfigs();
+//}
 
-	return (0);
+ServerConfig init_config(char *path) {
+    return ParseConfig(path).getConfig();
+}
+
+void start_server(const std::vector<ServerConfig> &configs) {
+    (void) configs;
+    // TODO: Create sockets, bind, listen, and start event loop
+}
+
+int main(int argc, char **argv) {
+    if (check_args(argc)) return 1;
+
+    try {
+        ServerConfig config = init_config(argv[1]);
+        print_conf(config);
+        std::vector<ServerConfig> configs;
+        configs.push_back(config);
+        start_server(configs);
+    }
+    catch (std::exception &e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+
+    return (0);
 }
